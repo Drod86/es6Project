c4d9ffcd393a24242e6b3ac5d57b963184b22aa1
Daniel Rodriguez <daniel.r.rodriguez86@gmail.com>
2023-07-30 14:53:05 -0500

    Strip project to get linters working

--- src/app.js
+++ src/app.js
@@ -1,27 +1,3 @@
 import './global.css';
 // Import the layout Elements
-import renderLayout from './view/layouts/layout.js';
-
-// Add Page Routing
-import { router, addRoute } from './controller/routes.js';
-
-const home = '<div>THIS IS THE HOME PAGE VIEW</div>';
-const about = '<div>this is the about page view</div>';
-
-// Render the Inital App Layout
-renderLayout(home);
-
-addRoute('', home); // param1 = name of the page, param 2 = the pages markup
-addRoute('about', about);
-router();
-
-// Controls
-
-// Grab Elements
-const navHome = document.querySelector('.home');
-const navAbout = document.querySelector('.about');
-const page = document.querySelector('.page');
-
-// Add Listeners
-navHome.addEventListener('click', () => { page.innerHTML = home; });
-navAbout.addEventListener('click', () => { page.innerHTML = about; });
+console.log(Testing);
