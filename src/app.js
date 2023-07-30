9739f8c25a89d46f53b80065215690ba5d2a115e
Daniel Rodriguez <daniel.r.rodriguez86@gmail.com>
2023-07-29 21:43:16 -0500

    Refresh

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 'use strict';
 import heading from "./components/heading.js";
-console.log('Hello world!');
 const BODY = document.querySelector('body');
-// const heading = document.createElement('h1');
 BODY.innerHTML = heading(1,'I Hope this works');
 BODY.appendChild(heading);
