51a0a40cfac500a172f31f6180e9d5ff94501cf7
Daniel Rodriguez <daniel.r.rodriguez86@gmail.com>
2023-07-30 16:58:43 -0500

    Add utils folder

---
+++ src/views/layout.js
@@ -0,0 +1,12 @@
+const renderLayout = (homePage) => {
+  const BODY = document.querySelector('body');
+  const app = `
+    <a href="#" class="home">HOME</a>
+    <a href="#about" class="about">ABOUT</a>
+    <div class="page">
+    ${homePage}
+    </div>`;
+  BODY.innerHTML = app;
+};
+
+export default renderLayout;
