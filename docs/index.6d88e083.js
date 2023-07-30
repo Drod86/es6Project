7547b07d6b32342ce56abe157d8bbce0ff37f8a9
Daniel Rodriguez <daniel.r.rodriguez86@gmail.com>
2023-07-29 19:37:23 -0500

    Correct browserlist in package.json

--- docs/index.6d88e083.js
+++ docs/index.6d88e083.js
@@ -1,2 +1,2 @@
 console.log("Hello world!");const o=document.querySelector("body"),e=document.createElement("h1");e.innerHTML="Helloooo World!",o.appendChild(e);
-//# sourceMappingURL=index.0c58ff51.js.map
+//# sourceMappingURL=index.6d88e083.js.map
