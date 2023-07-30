3d58ee86c6378004e7e756d2e6e60a2cd61d4661
Daniel Rodriguez <daniel.r.rodriguez86@gmail.com>
2023-07-29 19:07:03 -0500

    Initial file set up

---
+++ src/app.js
@@ -0,0 +1,6 @@
+'use strict';
+console.log('Hello world!');
+const BODY = document.querySelector('body');
+const heading = document.createElement('h1');
+heading.innerHTML = "Helloooo World!";
+BODY.appendChild(heading);
