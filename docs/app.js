c119d35feb8ca9db4c31ea07b7de0ebe0df31cc4
Daniel Rodriguez <daniel.r.rodriguez86@gmail.com>
2023-07-29 20:40:48 -0500

    Try fix for github pages

--- docs/app.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-console.log('Hello world!');
-const BODY = document.querySelector('body');
-const heading = document.createElement('h1');
-heading.innerHTML = "Helloooo World!";
-BODY.appendChild(heading);
